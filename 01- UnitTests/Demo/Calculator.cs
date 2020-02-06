04b18a552986fbaae7f60e7c887c0591a871ced9
Rafael Borges <rafaelbertelli89@gmail.com>
2020-02-05 21:36:45 -0300

    Add project files.

---
+++ 01- UnitTests/Demo/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo
+{
+    public class Calculator
+    {
+        public double Sum(double v1, double v2)
+        {
+            return v1 + v2;
+        }
+
+        public double Division(int v1, int v2)
+        {
+            return v1 / v2;
+        }
+
+       
+    }
+}
